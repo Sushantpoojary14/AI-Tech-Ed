a68b2291496018e892a7a6b2378c3b577d126e7c
Sushantpoojary14 <spoojari694@gmail.com>
2023-08-25 11:09:25 +0530

    Merge branch 'adminDash2' of https://github.com/Sushantpoojary14/AI-Tech-Ed into sushant

--- src/Pages/Admin/TestSeries/ViewProductDetail/ViewProductDetail.tsx
+++ src/Pages/Admin/TestSeries/ViewProductDetail/ViewProductDetail.tsx
@@ -76,63 +76,93 @@ const ViewProductDetail = () => {
     );
   };
 
+  const deleteSetMutation = useMutation({
+    mutationFn: async (setId: any) => {
+      console.log("mutation data", setId);
+      return await adminTokenAxios.delete(`/admin/delete-set/${setId}`);
+    },
+    onError: (error: any) => {
+      console.log("Error Deleting Set:", error);
+    },
+    onSuccess: (res: any) => {
+      console.log("Mutation Reponse", res?.response?.data?.Message);
+      setMessage(res?.response?.data?.Message);
+      handleAlertBoxOpen();
+    },
+  });
+
+  const handleDeleteSet = (setId: any) => {
+    console.log("delete", setId);
+    deleteSetMutation.mutate(setId);
+  };
+
   if (testSeries.isLoading) {
     return <div>Loading...</div>;
   }
   // console.log(testSeries?.data?.categories);
   return (
-    <Container maxWidth="lg">
-      <Stack marginTop={2} direction="row">
-        <Button
-          onClick={() => navigate(-1)}
-          size="small"
-          variant="contained"
-          color="primary"
-          sx={{ paddingRight: "1rem" }}
-        >
-          <ArrowBackIosNewRoundedIcon />
-          Back
-        </Button>
+    <>
+      <AlertBox
+        name={message}
+        type="success"
+        bol={open}
+        handleAlertBoxClose={handleAlertBoxClose}
+      />
 
-        <Stack
-          direction="row"
-          sx={{
-            // my: "18px",
-            justifyContent: "center",
-            mx: "auto",
-            pr: { lg: "100px", xs: "0px", sm: "100px", md: "100px" },
-          }}
-        >
-          <AssignmentOutlinedIcon
+      <Container maxWidth="lg">
+        <Stack marginTop={2} direction="row">
+          <Button
+            onClick={() => navigate(-1)}
+            size="small"
+            variant="contained"
+            color="primary"
+            sx={{ paddingRight: "1rem" }}
+          >
+            <ArrowBackIosNewRoundedIcon />
+            Back
+          </Button>
+
+          <Stack
+            direction="row"
             sx={{
-              height: "28px",
-              width: "28px",
-              color: "#FA8128",
-              mx: "8px",
-              my: "auto",
+              // my: "18px",
+              justifyContent: "center",
+              mx: "auto",
+              pr: { lg: "100px", xs: "0px", sm: "100px", md: "100px" },
             }}
-          />
-          <Header1 header="Package Detail" />
+          >
+            <AssignmentOutlinedIcon
+              sx={{
+                height: "28px",
+                width: "28px",
+                color: "#FA8128",
+                mx: "8px",
+                my: "auto",
+              }}
+            />
+            <Header1 header="Package Detail" />
+          </Stack>
         </Stack>
-      </Stack>
-      <Stack
-        mt={2}
-        // direction="row"
-        spacing={2}
-        // flexWrap="wrap"
-        // sx={{ width: "100%" }}
+        <Stack
+          mt={2}
+          // direction="row"
+          spacing={2}
+          // flexWrap="wrap"
+          // sx={{ width: "100%" }}
 
-        alignItems={"center"}
-      >
-        <SectionOne product={testSeries?.data} />
-        {/* <Stack direction="column" spacing={3} useFlexGap flexWrap="wrap"> */}
-        <SectionTwo
-          sets={testSeries?.data?.categories}
-          onSwitchToggle={handleSwitchToggle}
-        />
-        {/* </Stack> */}
-      </Stack>
-    </Container>
+          alignItems={"center"}
+        >
+          <SectionOne product={testSeries?.data} />
+          {/* <Stack direction="column" spacing={3} useFlexGap flexWrap="wrap"> */}
+          <SectionTwo
+            sets={testSeries?.data?.categories}
+            onSwitchToggle={handleSwitchToggle}
+            handleDeleteSet={handleDeleteSet}
+          />
+          {/* </Stack> */}
+        </Stack>
+      </Container>
+    </>
   );
 };
 
