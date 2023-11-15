abd559815512097bc2a63d2f8f75643ca1e3f3ea
Sushantpoojary14 <spoojari694@gmail.com>
2023-11-15 13:33:25 +0530

    .

--- src/Pages/Admin/TestSeries/ViewTestSeriesTopics/TopicView/ReadingTopic/ReadingTopicDetails.tsx
+++ src/Pages/Admin/TestSeries/ViewTestSeriesTopics/TopicView/ReadingTopic/ReadingTopicDetails.tsx
@@ -158,7 +158,7 @@ const ReadingTopicDetails = () => {
                 <div dangerouslySetInnerHTML={{ __html: item?.paragraph }} />
               </Box>
               <Divider />
-              <Box mt={2} mx={4}>
+              <Box mt={2} mx={4} display={"flex"} rowGap={6}>
                 {item?.question?.map((question: any, qIndex: any) => (
                   <Stack key={qIndex} sx={{ width: "100%" }}>
                     <div
